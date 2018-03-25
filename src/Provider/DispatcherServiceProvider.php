565b49e2542f6227e7431e7016bc7c7cbdce5e23
nilnice <nilnice@126.com>
2018-03-25 11:42:26 +0800

    : ) Initialize commit.

---
+++ src/Provider/DispatcherServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Nilnice\Phalcon\Provider;
+
+use Phalcon\DiInterface;
+use Phalcon\Mvc\Dispatcher;
+
+class DispatcherServiceProvider extends AbstractServiceProvider
+{
+    /**
+     * @var string
+     */
+    protected $name = 'dispatcher';
+
+    /**
+     * Register dispatcher service provider.
+     *
+     * @param mixed|null $parameter
+     *
+     * @return void
+     */
+    public function register($parameter = null): void
+    {
+        $di = $this->getDI();
+        $di->setShared($this->getName(), function () use ($di) {
+            /** @var \Phalcon\Config $route */
+            $route = $di->getShared('config')->get('route');
+            $dispatcher = new Dispatcher();
+            $dispatcher->setDefaultNamespace($route->get('namespace'));
+            $dispatcher->setDefaultController($route->get('controller'));
+            $dispatcher->setActionName($route->get('action'));
+
+            return $dispatcher;
+        });
+    }
+}
