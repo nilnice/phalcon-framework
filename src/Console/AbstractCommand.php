6a3a9d9e55f920a54223d60c862405cc5c8e0315
nilnice <nilnice@126.com>
2018-03-29 22:46:59 +0800

    : ) Update AbstractCommand class.

--- src/Console/AbstractCommand.php
+++ src/Console/AbstractCommand.php
@@ -3,8 +3,10 @@
 namespace Nilnice\Phalcon\Console;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
-abstract class AbstractConsole extends Command
+abstract class AbstractCommand extends Command
 {
     /**
      * @var string
@@ -32,18 +34,87 @@ abstract class AbstractConsole extends Command
     protected $output;
 
     /**
+     * An abstract method that will be called on every concrete command.
+     *
      * @return mixed
      */
-    protected function getInput()
+    abstract protected function goExecute();
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->input = $input;
+        $this->output = $output;
+        $this->goExecute();
+    }
+
+    /**
+     * @return \Symfony\Component\Console\Input\InputInterface
+     */
+    protected function getInput(): InputInterface
     {
         return $this->input;
     }
 
     /**
-     * @return mixed
+     * @return \Symfony\Component\Console\Output\OutputInterface
      */
-    protected function getOutput()
+    protected function getOutput(): OutputInterface
     {
         return $this->output;
+    }
+
+    /**
+     * This provides the arguments of this command.
+     *
+     * @return array
+     */
+    protected function getArgument()
+    {
+        return [];
+    }
+
+    /**
+     * This provides the options of this command.
+     *
+     * @return array
+     */
+    protected function getOption()
+    {
+        return [];
+    }
+
+    protected function configure()
+    {
+        $this->setName($this->name)
+            ->setDescription($this->description);
+
+        if ($this->help) {
+            $this->setHelp($this->help);
+        }
+
+        if (! empty($arguments = $this->getArgument())) {
+            foreach ($arguments as $argument) {
+                [0 => $name, 1 => $mode, 2 => $description] = $argument;
+                $default = $argument[3] ?? null;
+                $this->addArgument($name, $mode, $description, $default);
+            }
+        }
+
+        if (! empty($options = $this->getOption())) {
+            foreach ($options as $option) {
+                $this->addOption(
+                    $option[0] ?? null,
+                    $option[1] ?? null,
+                    $option[2] ?? null,
+                    $option[3] ?? null,
+                    $option[4] ?? null
+                );
+            }
+        }
+
+        return $this;
     }
 }
